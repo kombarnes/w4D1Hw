a23b82ad8c005b8df9e52a4d355f30fccef2ca35
Kourtney <barnes.kourt@gmail.com>
2022-01-27 22:05:50 -0500

    finished

--- style.js
+++ style.js
@@ -5,5 +5,27 @@ for (var i = 1; i < 11; i++) {
     myArray.push(i);
 }
 
+//2.
+var myArray = [];
+
+var i = 1;
+while(i < 11) {
+myArray.push(i);
+i++;
+}
+
+//3.
+var myArray = [];
+
+for (var i = 11; i < 22; i += 2) {
+  myArray.push(i);
+}
+
+//4.
+var myArray = [];
+
+for (var i = 15; i > 0; i -= 2) {
+  myArray.push(i);
+}
 
 
